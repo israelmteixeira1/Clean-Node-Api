5306b1df8169075c2ee5fe9363fc19788a84e7ac
israelmteixeira1 <israelmteixeira1@gmail.com>
2022-03-24 11:47:10 -0300

    feat: ensure requests returns json as default

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
