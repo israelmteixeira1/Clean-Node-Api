ee8d67d4c32b7b49d57f9b59c84ce4355e062ab6
israelmteixeira1 <israelmteixeira1@gmail.com>
2022-03-29 11:34:54 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
+import { MongoHelper } from "@/infra/db/mongodb/helpers/mongo-helper";
 import request from "supertest";
 import app from '../config/app'
 
 describe('Should return an account on success', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should enable CORS', async () => {
     await request(app)
       .post('/api/signup')
