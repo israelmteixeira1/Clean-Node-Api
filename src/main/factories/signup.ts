20511ffe2bc10c896f756baee8101ce3e1124d07
israelmteixeira1 <israelmteixeira1@gmail.com>
2022-04-25 22:25:45 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,14 +1,10 @@
-import { LogErrorRepository } from "@/data/protocols/log-error-repository";
 import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
 import { Controller } from "@/presentation/protocols";
 import { DbAddAccount } from "../../data/usecase/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
-import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
 import { LogControllerDecorator } from "../decorators/log";
-import { Validation } from "@/presentation/helpers/validators/validation";
-import { ValidationComposite } from "@/presentation/helpers/validators/validation-composite";
 import { makeSignupValidation } from "./signup-validation";
 
 export const makeSignupController = (): Controller => {
@@ -16,8 +12,7 @@ export const makeSignupController = (): Controller => {
   const encrypter = new BcryptAdapter(salt)
   const addAccountRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(encrypter, addAccountRepository)
-  const emailValidator = new EmailValidatorAdapter()
-  const signupController = new SignUpController(emailValidator, addAccount, makeSignupValidation())
+  const signupController = new SignUpController(addAccount, makeSignupValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signupController, logMongoRepository)
 }
---
+++ src/presentation/helpers/validators/email-validation.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError, MissingParamError } from "@/presentation/errors";
+import { EmailValidator } from "@/presentation/protocols/emailValidator";
+import { Validation } from "./validation";
+
+export class EmailValidation implements Validation {
+  private readonly fieldName
+  private readonly emailValidator: EmailValidator
+
+  constructor(fieldName: string, emailValidator: EmailValidator) {
+    this.fieldName = fieldName
+    this.emailValidator = emailValidator
+  }
+
+  validate(input: any): Error {
+    const isValid = this.emailValidator.isValid(input[this.fieldName])
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName)
+    }
+  }
+}
