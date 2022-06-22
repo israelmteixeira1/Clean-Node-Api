96b63c231daa3dcbe199ecf41792bbb31b3f94b8
Israel M Teixeira <israel@accon.com.br>
2022-06-21 22:07:59 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from "@/data/protocols/db/add-account-repository";
+import { LoadAccountByEmailRepository } from "@/data/protocols/db/load-account-by-email-repository";
 import { AccountModel } from "@/domain/models/account";
 import { AddAccountModel } from "@/domain/usecases/add-account";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.insertOne(account)
@@ -18,5 +19,25 @@ export class AccountMongoRepository implements AddAccountRepository {
       password: accountWithoutId.password
     }
     return MongoHelper.map(accountCreatedBody)
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+
+    if(!account) {
+      return null
+    }
+
+    const { _id, ...accountWithoutId } = account
+
+    const accountLoadBody = {
+      _id: _id,
+      email: accountWithoutId.email,
+      name: accountWithoutId.name,
+      password: accountWithoutId.password
+    }
+
+    return MongoHelper.map(accountLoadBody)
   }
 }
