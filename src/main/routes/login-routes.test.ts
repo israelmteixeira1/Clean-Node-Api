9816d360e688da89cfbc0ed614a1df260be88fe9
Israel M Teixeira <israel@accon.com.br>
2022-06-23 11:33:46 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper"
 import request from "supertest";
 import app from '../config/app'
 
-describe('Should return an account on success', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,15 +16,17 @@ describe('Should return an account on success', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should enable CORS', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Israel',
-        email: 'israel@accon.com.br',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Israel',
+          email: 'israel@accon.com.br',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
