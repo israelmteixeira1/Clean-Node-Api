69a685e363115ef36df97f38b6dddbb6cc1cf622
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-07-17 15:59:09 -0300

    refactor: update

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -3,7 +3,7 @@ import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
 import { makeSignupController } from "../factories/signup/signup-factory";
 
-export default (router: Router): void => {
+export default (router: Router) => {
   router.post('/signup', adaptRoute(makeSignupController()))
   router.post('/login', adaptRoute(makeLoginController()))
 }
