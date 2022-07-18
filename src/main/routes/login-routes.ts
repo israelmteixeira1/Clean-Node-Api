e186e06a14055e6113c9098891438405844c8414
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-07-18 12:09:27 -0300

    chore: remove dockerfile to use only docker-compose

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -3,7 +3,7 @@ import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
 import { makeSignupController } from "../factories/signup/signup-factory";
 
-export default (router: Router) => {
+export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()))
   router.post('/login', adaptRoute(makeLoginController()))
 }
