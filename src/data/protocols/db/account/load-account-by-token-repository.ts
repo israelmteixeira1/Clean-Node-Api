73c0b53750168d7bd43fac5765e926319f3931e4
Israel M Teixeira <israelmteixeira1@gmail.com>
2022-10-26 21:11:34 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

---
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -0,0 +1,6 @@
+import { AccountModel } from "../../../usecase/add-account/db-add-account-protocols";
+
+
+export interface LoadAccountByTokenRepository{
+    loadByToken (account: string, role?: string): Promise<AccountModel>
+}
