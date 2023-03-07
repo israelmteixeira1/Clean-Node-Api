b60769003ad46b051ddde64f63efa998d036b886
Israel M Teixeira <israelmteixeira1@gmail.com>
2023-03-07 14:52:56 -0300

    chore: commit teste

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -15,7 +15,7 @@ export class AddSurveyController implements Controller {
             const { question, answers } = httpRequest.body
             await this.addSurvey.add({
                 question,
-                answers
+                answers 
             })
             return noContent()
         } catch(error) {
